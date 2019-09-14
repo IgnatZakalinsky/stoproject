4e6d232933c2ec75a413c00d2295a2e922e9eac2
Anatoli1988 <energetik-ym191@mail.ru>
2019-09-14 18:42:10 +0300

    Create store

---
+++ front/src/Redux/Store.js
@@ -0,0 +1,15 @@
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import  thunkMiddleware from "redux-thunk"
+import {applyMiddleware, combineReducers, createStore} from "redux";
+
+
+let reducers = combineReducers({
+    // profilePage: profileReducer,
+    // dialogsPage: dialogsReducer,
+    // sidebar: sidebarReducer,
+    
+})
+
+let store = createStore(reducers,applyMiddleware(thunkMiddleware))
+
+export default store
