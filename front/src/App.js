d918db474b543ff0b1833c25a83718fb50a99800
Ignat <ai73a@yandex.ru>
2019-09-14 19:16:57 +0300

    global fix

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import STO from "./components/STO";
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./Redux/Store";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <STO/>
+                <Provider store={store}>
+                    <STO/>
+                </Provider>
             </BrowserRouter>
         </div>
     );
--- front/src/Redux/Store.js
+++ front/src/Redux/Store.js
@@ -1,6 +1,5 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import  thunkMiddleware from "redux-thunk"
-import {applyMiddleware, combineReducers, createStore} from "redux";
 import carsReducer from "../reducers/carsReducer";
 import clientReducer from "../reducers/clientsRreducer";
 import ordersReducer from "../reducers/ordersReducer";
