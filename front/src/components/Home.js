2249323d3955fac2a4370d97ec53d1836755e5db
Ignat <ai73a@yandex.ru>
2019-09-15 10:47:03 +0300

    create navbar Home

---
+++ front/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+function Home() {
+    return (
+        <div className="Home" style={{background: 'lime'}}>
+            STO <NavLink to={'/clients'}><button>Clients</button></NavLink>
+        </div>
+    );
+}
+
+export default Home;
