8004238daa39c3c7b8af267f9a49d86d13591d4a
Anatoli1988 <energetik-ym191@mail.ru>
2019-09-16 14:21:42 +0300

    Layout Client component

--- front/src/components/clients/Client.js
+++ front/src/components/clients/Client.js
@@ -3,14 +3,17 @@ import styles from './Client.module.css'
 import {NavLink} from "react-router-dom";
 
 function Client(props) {
-    const data = Object.entries(props).map((d, i) => <div key={i}>
-        <span>{d[0]}</span> : <span>{d[1]}</span>
-    </div>)
+    const data = Object.entries(props).map((d, i) =><div className={styles.column} key={i}>
+        <span className={styles.key}>{d[0]}</span>  <span className={styles.value}>{d[1]}</span>
+        </div>
+   )
 
     return (
         <div className="Client">
             <NavLink to={'/clientProfile/' + props.id}>
+                <div className={styles.hhh}>
                 {data}
+                </div>
                 {/*}div><span>Name: </span><span>{props.firstName}</span></div>
             <span>Last Name: </span><div>{props.lastName}</div>
             <span>Year: </span><div>{props.dob}</div>
